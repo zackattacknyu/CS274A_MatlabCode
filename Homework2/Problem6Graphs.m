ad866fbef759300e2adab6cc090f3ec03b034265
zackattacknyu <zackattacknyu@gmail.com>
2014-01-21 00:04:05 -0800

    finished code for problem 6

---
+++ Homework2/Problem6Graphs.m
@@ -0,0 +1,8 @@
+%For part a
+binlikelihood(5,10);
+
+%For part b
+binlikelihood(50,100);
+
+%For part c
+binlikelihood(5,50);
---
+++ Homework2/binlikelihood.m
@@ -0,0 +1,12 @@
+function [] = binlikelihood(rValue,nValue)
+%BINLIKELIHOOD Plots the likelihood function for the binary distribution
+%   This plots the parameter p, the probability, on the x-axis
+%   The y axis is the likelihood
+%   The equation is L(p) = p^r * (1-p)^(n-r)
+
+pValues = [0:1/1000:1];
+L_Values = (pValues.^rValue).*((1-pValues).^(nValue-rValue));
+plot(pValues,L_Values);
+
+end
+
