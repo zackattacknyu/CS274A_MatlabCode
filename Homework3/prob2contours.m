7a368afa4c65fa13d8ea9f2c3c498da63130ff4b
zackattacknyu <zackattacknyu@gmail.com>
2014-02-04 13:13:49 -0800

    completely fixed code

--- Homework3/prob2contours.m
+++ Homework3/prob2contours.m
@@ -7,10 +7,7 @@
 
 %}
 
-theta_1 = 0:1/100:1;
-theta_2 = 0:1/100:1;
-
-part = 1;
+part = 4;
 
 if(part == 4)
     alpha_1 = 8;
@@ -33,43 +30,35 @@ end
 
 p_theta = eye(101);
 
-for row = 1:101,
-   for col = 1:101,
-       theta_1_current = theta_1(row);
-       theta_2_current = theta_2(col);
-       theta_3_current = 1- (theta_1_current + theta_2_current);
-       
-       %does the normal likelihood
-       
-       if(theta_3_current >= 0)
-           p_theta(row,col) = theta_1_current^(alpha_1-1)*...
-                theta_2_current^(alpha_2-1)*...
-                theta_3_current^(alpha_3-1);
-       else
-           p_theta(row,col) = 0;
-       end
-      
-       %does the log likelihood
-       %{
-       if(theta_3_current >= 0)
-           p_theta(row,col) = (alpha_1-1)*log(theta_1_current)+...
-                (alpha_2-1)*log(theta_2_current)+...
-                (alpha_3-1)*log(theta_3_current);
-       else
-           p_theta(row,col) = 0;
-       end
-        %}
-      
+%The code here will be used
+%   http://www.mathworks.com/help/matlab/ref/contour.html
+%
+
+theta_1 = linspace(0,1);
+theta_2 = 1-theta_1;
+
+[THETA_1,THETA_2] = meshgrid(theta_1,theta_2);
+THETA_3 = 1-(THETA_1+THETA_2);
+sizeVector = size(THETA_3);
+THETA_3(THETA_3<0) = 0;
+P_THETA = (THETA_1.^(alpha_1-1))...
+    .*(THETA_2.^(alpha_2-1))...
+    .*(THETA_3.^(alpha_3-1));
+for row = 1:sizeVector(1),
+   for col = 1:sizeVector(2),
+      if(THETA_3(row,col) <= 0)
+         P_THETA(row,col) = 0; 
+      end
    end
 end
 
 beta = alpha_1 + alpha_2 + alpha_3;
-mean_alpha1 = alpha_1/beta
-mean_alpha2 = alpha_2/beta
-mean_alpha3 = alpha_3/beta
+mean_alpha1 = alpha_1/beta;
+mean_alpha2 = alpha_2/beta;
+mean_alpha3 = alpha_3/beta;
 
-var_alpha1 = alpha_1*(beta - alpha_1)/(beta^2*(beta+1))
-var_alpha2 = alpha_2*(beta - alpha_2)/(beta^2*(beta+1))
-var_alpha3 = alpha_3*(beta - alpha_3)/(beta^2*(beta+1))
+var_alpha1 = alpha_1*(beta - alpha_1)/(beta^2*(beta+1));
+var_alpha2 = alpha_2*(beta - alpha_2)/(beta^2*(beta+1));
+var_alpha3 = alpha_3*(beta - alpha_3)/(beta^2*(beta+1));
 
-contour(theta_2,theta_1,p_theta)
+contour(THETA_1,THETA_2,P_THETA)
