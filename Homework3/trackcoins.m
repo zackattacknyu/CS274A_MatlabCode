90efd8d84d1474d580d2029582e9d02d3cec44cd
zackattacknyu <zackattacknyu@gmail.com>
2014-02-04 15:11:04 -0800

    got initial track coins code working

--- Homework3/trackcoins.m
+++ Homework3/trackcoins.m
@@ -11,14 +11,35 @@ function trackcoins( p, q , N, alpha, beta, k)
 % k: integer indicating the window size for "forgetting"
 % simulate a sequence of coins:
 pheads = p;
+
+%sets r_1
+remainingTosses = geornd(q);
+
+%preallocating data and prob arrays
+data = 1:1:N;
+prob = 1:1:N;
+
+%toss the coin
 for i=1:N
-% toss the coin
-data(i) = rand < pheads; % generates a "1" with probability pheads
-prob(i) = pheads;
-fprintf('%d',data(i));
-% switch coins or not?
-%.....your code goes here for the rest of this function......
-% (its convenient to put in an fprintf(’\n’); whenever the coins switch)
+    
+    %find out if the probability needs to be switched
+    if(remainingTosses <= 0)
+        
+        %sets the next r value
+        remainingTosses = geornd(q);
+        
+        %switch the probability
+        pheads = 1 - pheads;
+        fprintf('\n');
+    end
+    
+    % toss the coin
+    data(i) = rand < pheads; % generates a "1" with probability pheads
+    prob(i) = pheads;
+    fprintf('%d ',data(i));
+    
+    %decrement the remaining tosses before the switch
+    remainingTosses = remainingTosses - 1;
 end
 fprintf('\n');
 % plot the coin tosses, true probabilities, and estimated posterior mean
@@ -26,6 +47,6 @@ xvalues = 1:N;
 figure; plot(xvalues,data,'r.'); % plot data, the binary coin tosses (0 or 1)
 hold on;
 plot(xvalues,prob,'b*'); % plot the true coin probability for each turn
-plot(xvalues,mpe,'g:','linewidth',3); % plot the estimated posterior mean
+%plot(xvalues,mpe,'g:','linewidth',3); % plot the estimated posterior mean
 axis([0 N+1 -0.1 1.1]);
 
