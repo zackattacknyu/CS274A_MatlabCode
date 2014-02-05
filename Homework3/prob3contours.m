98c668e021b9ef96a9f51c9efedf0e1614d37a6c
zackattacknyu <zackattacknyu@gmail.com>
2014-02-04 17:40:25 -0800

    got contours for part 3 initially working

--- Homework3/prob3contours.m
+++ Homework3/prob3contours.m
@@ -7,26 +7,6 @@
 
 %}
 
-part = 1;
-
-if(part == 4)
-    alpha_1 = 8;
-    alpha_2 = 1;
-    alpha_3 = 1;
-elseif(part == 3)
-    alpha_1 = 0.1;
-    alpha_2 = 0.1;
-    alpha_3 = 0.1;    
-elseif(part == 2)
-    alpha_1 = 10;
-    alpha_2 = 10;
-    alpha_3 = 10;
-elseif(part == 1)
-    alpha_1 = 1;
-    alpha_2 = 1;
-    alpha_3 = 1;
-end
-
 %for Part 1 Data
 r_1 = 8;
 r_2 = 1;
@@ -44,21 +24,46 @@ r_3 = 1;
 %
 
 theta_1 = linspace(0,1);
-theta_2 = 1-theta_1;
-
+theta_2 = linspace(0,1);
 [THETA_1,THETA_2] = meshgrid(theta_1,theta_2);
 THETA_3 = 1-(THETA_1+THETA_2);
 sizeVector = size(THETA_3);
 THETA_3(THETA_3<0) = 0;
-P_THETA = (THETA_1.^(r_1 + alpha_1-1))...
-    .*(THETA_2.^(r_2 + alpha_2-1))...
-    .*(THETA_3.^(r_3 + alpha_3-1));
-for row = 1:sizeVector(1),
-   for col = 1:sizeVector(2),
-      if(THETA_3(row,col) < 0)
-         P_THETA(row,col) = 0; 
-      end
-   end
+
+for part = 1:4,
+    if(part == 4)
+        alpha_1 = 8;
+        alpha_2 = 1;
+        alpha_3 = 1;
+    elseif(part == 3)
+        alpha_1 = 0.1;
+        alpha_2 = 0.1;
+        alpha_3 = 0.1;
+    elseif(part == 2)
+        alpha_1 = 10;
+        alpha_2 = 10;
+        alpha_3 = 10;    
+    elseif(part == 1)
+        alpha_1 = 1;
+        alpha_2 = 1;
+        alpha_3 = 1;
+    end
+   
+    P_THETA = (THETA_1.^(r_1 + alpha_1-1))...
+        .*(THETA_2.^(r_2 + alpha_2-1))...
+        .*(THETA_3.^(r_3 + alpha_3-1));
+    for row = 1:sizeVector(1),
+       for col = 1:sizeVector(2),
+          if(THETA_3(row,col) <= 0)
+             P_THETA(row,col) = 0; 
+          end
+       end
+    end 
+    
+    subplot(2,2,part);
+    contour(THETA_1,THETA_2,P_THETA)
+    
+    
 end
 
 beta = alpha_1 + alpha_2 + alpha_3;
@@ -70,4 +75,3 @@ var_alpha1 = alpha_1*(beta - alpha_1)/(beta^2*(beta+1));
 var_alpha2 = alpha_2*(beta - alpha_2)/(beta^2*(beta+1));
 var_alpha3 = alpha_3*(beta - alpha_3)/(beta^2*(beta+1));
 
-contour(THETA_1,THETA_2,P_THETA)
