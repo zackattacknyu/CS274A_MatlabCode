e8c1f3d7bd38cd4e02c7306bcd0b8b286177a9d0
zackattacknyu <zackattacknyu@gmail.com>
2014-02-28 17:43:21 -0800

    got basic clustering working for the kmeans part of assignment 5

--- Homework5/initScript.m
+++ Homework5/initScript.m
@@ -1,3 +1,46 @@
 load('dataset1.txt');
 load('dataset2.txt');
 load('dataset3.txt');
+
+plot(dataset1(:,1),dataset1(:,2),'r.');
+%plot(dataset2(:,1),dataset2(:,2));
+
+%does k-means
+dataset = dataset1;
+k=2;
+datasetSize = size(dataset);
+numPoints = datasetSize(1);
+
+randOrder = randperm(numPoints);
+meanRows = randOrder(1:k);
+
+%assign the cluster rows randomly
+currentClusters = zeros(k,2);
+for row = 1:k
+    currentClusters(row,:) = dataset(meanRows(row),:);
+end
+
+%loop through the data points finding the closest cluster
+currentClusterAssignments = zeros(numPoints,1);
+currentClusterAssn = 1;
+cluster1Rows = [];
+cluster2Rows = [];
+for dataPoint = 1:numPoints
+    currentMinDistance = 20;
+   for cluster = 1:k
+       currentDistance = norm(currentClusters(cluster,:)-dataset(dataPoint,:));
+       if(currentDistance < currentMinDistance)
+          currentClusterAssignments(dataPoint,1) = cluster; 
+          currentClusterAssn = cluster;
+       end
+       currentMinDistance = currentDistance;
+   end
+   
+   if(currentClusterAssn == 1)
+      cluster1Rows = [cluster1Rows;dataset(dataPoint,:)]; 
+   elseif(currentClusterAssn == 2)
+       cluster2Rows = [cluster2Rows;dataset(dataPoint,:)]; 
+   end
+end
+
+plot(cluster1Rows(:,1),cluster1Rows(:,2),'r.',cluster2Rows(:,1),cluster2Rows(:,2),'b.');
