8c918bffbee5807e6958c997fb342bde74b7ec58
zackattacknyu <zackattacknyu@gmail.com>
2014-03-01 17:08:51 -0800

    improved the loop tremendously

--- Homework5/initScript.m
+++ Homework5/initScript.m
@@ -16,6 +16,7 @@ meanRows = randOrder(1:k);
 
 %assign the cluster rows randomly
 currentClusters = zeros(k,2);
+currentClusterDistances = zeros(k,1);
 for row = 1:k
     currentClusters(row,:) = dataset(meanRows(row),:);
 end
@@ -23,37 +24,35 @@ end
 
 currentClusterAssignments = zeros(numPoints,1);
 
-for iteration = 1:3
+for iteration = 1:4
     currentNumChanged = 0;
     cluster1Rows = [];
     cluster2Rows = [];
     
     %loop through the data points finding the closest cluster for each point
     for dataPoint = 1:numPoints
-        currentMinDistance = 20;
        for cluster = 1:k
-           currentDistance = norm(currentClusters(cluster,:)-dataset(dataPoint,:));
-           if(currentDistance < currentMinDistance)
-               if(currentClusterAssignments(dataPoint,1) ~= cluster)
-                   currentClusterAssignments(dataPoint,1) = cluster;
-                   currentNumChanged = currentNumChanged + 1;
-               end
-              currentMinDistance = currentDistance;
-           end
-           
+           currentClusterDistances(cluster) = norm(currentClusters(cluster,:)-dataset(dataPoint,:));
+       end
+       
+       [currentMinDistance,bestCluster] = min(currentClusterDistances);
+       
+       if(bestCluster ~= currentClusterAssignments(dataPoint))
+           currentClusterAssignments(dataPoint) = bestCluster;
+           currentNumChanged = currentNumChanged + 1;
        end
 
-       if(currentClusterAssignments(dataPoint,1) == 1)
+       if(currentClusterAssignments(dataPoint) == 1)
           cluster1Rows = [cluster1Rows;dataset(dataPoint,:)]; 
-       elseif(currentClusterAssignments(dataPoint,1) == 2)
+       elseif(currentClusterAssignments(dataPoint) == 2)
            cluster2Rows = [cluster2Rows;dataset(dataPoint,:)]; 
        end
     end
 
-    
+    figure
     plot(cluster1Rows(:,1),cluster1Rows(:,2),'r.',...
         cluster2Rows(:,1),cluster2Rows(:,2),'b.');
-    figure
+    
     
     %reassign the clusters
     currentClusters(1,:) = [mean(cluster1Rows(:,1)) mean(cluster1Rows(:,2))];
