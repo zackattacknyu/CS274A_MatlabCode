e583a9a658e7096d92cf4d95b262d329bcb12090
zackattacknyu <zackattacknyu@gmail.com>
2014-03-02 20:05:17 -0800

    furthur modularized the code

--- Homework5/computeMemberProbs.m
+++ Homework5/computeMemberProbs.m
@@ -3,18 +3,8 @@ function [ memberProbs ] = computeMemberProbs( dataset, alphaValues, K, muVector
 
 datasetSize = size(dataset);
 numPoints = datasetSize(1);
-numDimensions = datasetSize(2);
 memberProbs = zeros([numPoints K]);
-pVector = zeros([numPoints K]);
-for k = 1:K
-    for dataPt = 1:numPoints
-        diffVector = dataset(dataPt,:)-muVector(k,:);
-        exponent = (diffVector/sigmaVector(:,:,k))*transpose(diffVector);
-        denominator = ((2*pi)^(numDimensions/2))*...
-            sqrt(det(sigmaVector(:,:,k)));
-        pVector(dataPt,k) = (1/denominator)*exp(-0.5*exponent);
-    end 
-end
+pVector = computePvector(dataset, K, muVector, sigmaVector);
 for k = 1:K
     for dataPt = 1:numPoints
         memberProbs(dataPt,k) = (pVector(dataPt,k)*alphaValues(k))/...
