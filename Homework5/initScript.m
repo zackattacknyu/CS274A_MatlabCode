6d54460ea6da9e0d2570d910dd09e0c38a3da27a
zackattacknyu <zackattacknyu@gmail.com>
2014-03-02 18:24:10 -0800

    began method 1 initialization for EM algorithm

--- Homework5/initScript.m
+++ Homework5/initScript.m
@@ -6,14 +6,28 @@ load('dataset3.txt');
 %plot(dataset2(:,1),dataset2(:,2),'r.');
 %plot(dataset3(:,1),dataset3(:,2),'r.');
 
+
+
 dataset = dataset1;
 k=2;
 maxiterations = 10;
 r = 10;
 
+%{
+UNCOMMENT AFTER EM IS CODED
 [finalCluster1Rows, finalCluster2Rows] = kMeansCluster(dataset,k,r,maxiterations);
 
 figure
 plot(finalCluster1Rows(:,1),finalCluster1Rows(:,2),'.',...
         finalCluster2Rows(:,1),finalCluster2Rows(:,2),'o');
 
+%}
+
+%method 1
+K = 2;
+datasetSize = size(dataset);
+numPoints = datasetSize(1);
+memberProbs = rand([numPoints K]);
+memberProbs = diag(sum(transpose(memberProbs)))\memberProbs; %normalize each row
+Nvector = sum(memberProbs);
+alphaValues = Nvector./numPoints;
