8235661a356594769498bae962f58b7fdce12933
zackattacknyu <zackattacknyu@gmail.com>
2014-03-02 19:05:08 -0800

    got the init M step computed correctly

--- Homework5/initScript.m
+++ Homework5/initScript.m
@@ -27,7 +27,23 @@ plot(finalCluster1Rows(:,1),finalCluster1Rows(:,2),'.',...
 K = 2;
 datasetSize = size(dataset);
 numPoints = datasetSize(1);
+numDimensions = datasetSize(2);
 memberProbs = rand([numPoints K]);
 memberProbs = diag(sum(transpose(memberProbs)))\memberProbs; %normalize each row
 Nvector = sum(memberProbs);
 alphaValues = Nvector./numPoints;
+
+%init M step
+muVector = zeros([K numDimensions]);
+for k = 1:K
+    muVector(k,:) = (sum(diag(memberProbs(:,k))*dataset))./Nvector(k);
+end
+sigmaVector = zeros([numDimensions numDimensions K]);
+for k = 1:K
+    currentSum = zeros([numDimensions numDimensions]);
+    for dataPt = 1:numPoints
+        diffVector = dataset(dataPt,:)-muVector(k,:);
+        currentSum = currentSum + (transpose(diffVector)*diffVector).*memberProbs(dataPt,k);
+    end
+   sigmaVector(:,:,k) = currentSum./Nvector(k);  
+end
