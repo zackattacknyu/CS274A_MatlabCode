d022cdb7f50e47ee774c55422df6de197239c22c
zackattacknyu <zackattacknyu@gmail.com>
2014-03-11 16:25:24 -0700

    finished first draft of homework

--- Homework6/prob4plot.m
+++ Homework6/prob4plot.m
@@ -3,37 +3,16 @@ means = [1 1;4 4];
 boundsY = 0:1/10:5;
 sizeBounds = size(boundsY);
 numYvals = sizeBounds(2);
-xVals = (ones(1,numYvals))*2.5;
+
+%for part 1
+%xVals = (ones(1,numYvals))*2.5;
+
+%for part 2
+xVals = (ones(1,numYvals))*((15-2*log(4))/6);
+
 
 figure
 hold on;
 plot(xVals,boundsY,'r-');
 plot(means(:,1),means(:,2),'bx','LineWidth',4);
 hold off;
-
-
-%figures out the numbers for part 2
-%{
-logPc_1 = 0.8;
-logPc_2 = 0.2;
-v = -2*(logPc_2 - logPc_1);
-mu1 = [1;1];
-mu2 = [4;4];
-covMat = [1 1;1 4];
-uVal = v - transpose(mu2)*covMat*mu2 + transpose(mu1)*covMat*mu1;
-mu0 = mu1-mu2;
-m1 = transpose(mu0)*covMat;
-m2 = covMat*mu0;
-wVector = m1 + transpose(m2);
-w0vector = -1*uVal;
-
-xVals2 = -3:1/100:10;
-yVals2 = -(wVector(1).*xVals + w0vector)/wVector(2);
-
-figure;
-hold on;
-plot(xVals2,yVals2,'r-');
-plot(xVals,yVals,'g-');
-plot(means(:,1),means(:,2),'bx','LineWidth',4);
-hold off;
-%}
