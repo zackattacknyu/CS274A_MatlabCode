d2ee8a776b3bc7594bde78c46c3169571e4f7dac
zackattacknyu <zackattacknyu@gmail.com>
2014-03-11 12:10:43 -0700

    got more of problem 2 done

--- Homework6/plots.m
+++
@@ -1,3 +0,0 @@
-xVals = 0:0.01:5;
-yVals = exp(-xVals);
-plot(xVals,yVals);
---
+++ Homework6/prob2plot.m
@@ -0,0 +1,16 @@
+xVals = 1:0.01:5;
+yVals1 = .5*exp(-xVals);
+yVals2 = ((xVals >= 2)&(xVals<=4))*0.25;
+boundsY = -0.02:1/10:0.3;
+sizeBounds = size(boundsY);
+numYvals = sizeBounds(2);
+boundX = ones(1,numYvals);
+root1 = 2;
+root2 = 4;
+bound1X = boundX.*root1;
+bound2X = boundX.*root2;
+plot(xVals,yVals2,'g-',xVals,yVals1,'b-',bound1X,boundsY,'r--',bound2X,boundsY,'r--');
+xlabel('X value');
+ylabel('Probability');
+title('Probability of different class labels');
+legend('Class 1 Probability','Class 2 Probability','Decision Boundary');
