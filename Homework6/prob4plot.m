348b277c262ae04e81ea1c91e4260d13a4bcf6ed
zackattacknyu <zackattacknyu@gmail.com>
2014-03-11 15:50:38 -0700

    fixed problem 4 part 1

--- Homework6/prob4plot.m
+++ Homework6/prob4plot.m
@@ -1,17 +1,19 @@
 means = [1 1;4 4];
 
-xVals = -3:1/100:10;
-yVals = (-12.*xVals + 105)/30;
+boundsY = 0:1/10:5;
+sizeBounds = size(boundsY);
+numYvals = sizeBounds(2);
+xVals = (ones(1,numYvals))*2.5;
 
-%{
 figure
 hold on;
-plot(xVals,yVals,'r-');
+plot(xVals,boundsY,'r-');
 plot(means(:,1),means(:,2),'bx','LineWidth',4);
 hold off;
-%}
+
 
 %figures out the numbers for part 2
+%{
 logPc_1 = 0.8;
 logPc_2 = 0.2;
 v = -2*(logPc_2 - logPc_1);
@@ -34,3 +36,4 @@ plot(xVals2,yVals2,'r-');
 plot(xVals,yVals,'g-');
 plot(means(:,1),means(:,2),'bx','LineWidth',4);
 hold off;
+%}
